ba2d3dc94588281cc3806715efbcd82e8928fd4e
Anton Rissanen <anton.rissanen@reaktor.fi>
2015-02-28 20:17:05 +0200

    Add ability to see history

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 var React = require('react')
+var Immutable = require('immutable')
 var {nextPieces, currentPiece, next} = require('./nextPiece')
 var Bacon = require('baconjs')
 var NextPiecesView = require('./views/nextPiecesView')
@@ -13,14 +14,35 @@ var worldStream = Bacon.combineTemplate({
   playField: playFieldStream
 })
 
+var history = Immutable.List.of()
+
+var worldStreamWithHistory = worldStream.map(Immutable.Map).map(function(world) {
+  history = history.push(world)
+  return Immutable.Map({
+    world,
+    history
+  })
+})
+
 var Main = React.createClass({
+  getInitialState: function() { return { selectedWorldFromHistory: undefined } },
+  onSlide: function() {
+    var historyNth = Number(this.refs.historySlider.getDOMNode().value)
+    var world = this.props.history.get(historyNth)
+    this.setState({ selectedWorldFromHistory: world })
+  },
   render: function() {
+
+    var world = this.state.selectedWorldFromHistory ? this.state.selectedWorldFromHistory : this.props.world
+
     return <div>
-      <NextPiecesView pieces={this.props.world.nextPieces} />
+      <p>History size: {this.props.history.size}</p>
+      <p><input type="range" min="0" max={this.props.history.size} onChange={this.onSlide} ref="historySlider" /></p>
+      <NextPiecesView pieces={world.get('nextPieces')} />
       <PlayField
-        playField={this.props.world.playField}
-        currentPiece={this.props.world.currentPiece}
-        environment={this.props.world.environment}
+        playField={world.get('playField')}
+        currentPiece={world.get('currentPiece')}
+        environment={world.get('environment')}
       />
     </div>
   }
@@ -29,14 +51,18 @@ var Main = React.createClass({
 var Initializer = React.createClass({
   componentDidMount: function() {
     var component = this
-    this.props.worldStream.onValue((world) => component.setState({ world }))
+    this.props.worldStreamWithHistory.onValue(function(x) {
+      component.setState({
+        world: x.get('world'), history: x.get('history') })
+      }
+    )
   },
   render: function() {
-    return this.state && this.state.world ? <Main world={this.state.world} /> : <div />
+    return this.state && this.state.world ? <Main world={this.state.world} history={this.state.history} /> : <div />
   }
 })
 
 React.render(
-  <Initializer worldStream={worldStream} />,
+  <Initializer worldStreamWithHistory={worldStreamWithHistory} />,
   document.getElementById('main')
 )
